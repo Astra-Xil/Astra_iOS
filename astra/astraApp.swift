ac565a95eecd4071631c85aa11d365b7dd8c3e0d
XIL(しーる) <120433484+xil-0828@users.noreply.github.com>
2025-12-22 05:32:17 +0900

    Initial Commit

---
+++ astra/astraApp.swift
@@ -0,0 +1,17 @@
+//
+//  astraApp.swift
+//  astra
+//
+//  Created by Xil on 2025/12/22.
+//
+
+import SwiftUI
+
+@main
+struct astraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
